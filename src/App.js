824a1bf23bd443a263ccb6ec6b42ed03e647c08c
Anish Pawaskar <anishpawaskar@gmail.com>
2023-05-04 03:11:32 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { IntroSectin } from './components/IntroSectin';
 import { Navbar } from './components/Navbar';
+import { Posts } from './components/Posts';
 
 function App() {
   return (
     <div className="ghost-site-layout">
       <Navbar />
       <Header />
+      <IntroSectin />
+      <Posts />
+      <Footer />
     </div>
   );
 }
